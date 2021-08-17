83c0cc0bb45195854b49f8683b2a7ea6d0d25c85
Santanu Sinha <santanu.sinha@gmail.com>
2021-08-17 13:29:52 +0530

    Added healthcheck

---
+++ event-ingester-server/src/main/java/io/appform/eventingester/server/healthchecks/KafkaHealthcheck.java
@@ -0,0 +1,39 @@
+package io.appform.eventingester.server;
+
+import io.appform.eventingester.server.kafka.KafkaConfig;
+import lombok.val;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import ru.vyarus.dropwizard.guice.module.installer.feature.health.NamedHealthCheck;
+
+import javax.inject.Inject;
+import java.util.Properties;
+
+/**
+ * Checks kafka
+ */
+public class KafkaHealthcheck extends NamedHealthCheck {
+    private final AdminClient adminClient;
+
+    @Inject
+    public KafkaHealthcheck(KafkaConfig config, AdminClient adminClient) {
+        val properties = new Properties();
+        properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, config.getBootstrapHosts());
+        this.adminClient = AdminClient.create(properties);
+    }
+
+    @Override
+    public String getName() {
+        return "kafka-check";
+    }
+
+    @Override
+    protected Result check() throws Exception {
+        val response = adminClient.describeCluster();
+        return (response.nodes().get().isEmpty()
+                || response.clusterId() == null
+                || response.controller().get() == null)
+               ? Result.unhealthy("Kafka cluster seems to be down")
+               : Result.healthy();
+    }
+}
